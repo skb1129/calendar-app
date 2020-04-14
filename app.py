50b3a05d6d8a08c9f330b6e703240f8c5e29d18d
Surya Kant Bansal <suryakantbansal97@gmail.com>
2020-04-14 17:44:38 +0530

    define login and registration endpoints

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, jsonify, abort
+from flask_bcrypt import Bcrypt
+from sqlalchemy.exc import IntegrityError
+
+from .auth import encode_auth_token
+from .models import setup_db, User, db_drop_and_create_all
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+setup_db(app)
+db_drop_and_create_all()
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    user = User(
+        username=request.json.get("username"),
+        password=bcrypt.generate_password_hash(request.json.get("password")).decode("UTF-8"),
+        email=request.json.get("email")
+    )
+    try:
+        user.insert()
+    except IntegrityError as e:
+        return abort(400, "BAD_REQUEST: Unable to register user.")
+    access_token = encode_auth_token(user.username)
+    return jsonify({
+        "success": True,
+        "access_token": access_token
+    }), 201
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username")
+    password = request.json.get("password")
+    if not username or not password:
+        return abort(400, "BAD_REQUEST: \"username\" or \"password\" not provided.")
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return abort(404, "NOT_FOUND: User not found")
+    if not bcrypt.check_password_hash(user.password, password):
+        return abort(409, "CONFLICT: Wrong Password")
+    access_token = encode_auth_token(username)
+    return jsonify({
+        "success": True,
+        "access_token": access_token
+    }), 202
+
+
+if __name__ == '__main__':
+    app.run()
