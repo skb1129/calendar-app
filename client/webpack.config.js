847fef664a86469b9304aca016ced69942f5f62c
Surya Kant Bansal <suryakantbansal97@gmail.com>
2020-04-16 13:22:29 +0530

    fix server url in webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 
-const SERVER = "https://localhost:5000";
+const SERVER = "http://localhost:5000";
 
 module.exports = ({ mode } = { mode: "development" }) => ({
   mode,
