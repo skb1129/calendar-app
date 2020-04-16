adf710dfdcec9c77c97c7dd527a09f3e786159c1
Surya Kant Bansal <suryakantbansal97@gmail.com>
2020-04-16 21:32:44 +0530

    display user data on home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,6 +3,10 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
+import Divider from "@material-ui/core/Divider";
+import { blueGrey } from "@material-ui/core/colors";
 
 import { useAuth } from "../contexts/AuthContext";
 import TopBar from "./TopBar";
@@ -10,9 +14,17 @@ import TopBar from "./TopBar";
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(2),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+  },
+  avatar: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+    marginRight: theme.spacing(3),
+    color: theme.palette.getContrastText(blueGrey[500]),
+    backgroundColor: blueGrey[500],
+  },
+  divider: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
   },
 }));
 
@@ -23,10 +35,28 @@ function Home() {
   return (
     <>
       <TopBar />
-      <Container component="main" maxWidth="xs">
-        <div className={classes.paper}>
-          <Typography variant="h2">{`${user.firstName} ${user.lastName}`}</Typography>
-        </div>
+      <Container component="main">
+        <Grid container direction="row" alignItems="center" className={classes.paper}>
+          <Grid item>
+            <Avatar className={classes.avatar}>
+              {user.firstName && user.firstName.charAt(0)}
+              {user.lastName && user.lastName.charAt(0)}
+            </Avatar>
+          </Grid>
+          <Grid item>
+            <Typography variant="h4">{`${user.firstName} ${user.lastName}`}</Typography>
+            <Typography variant="subtitle1">@{user.username}</Typography>
+          </Grid>
+        </Grid>
+        <Divider className={classes.divider} />
+        {user.email && (
+          <Typography variant="body1">
+            <Typography component="span" variant="h6">
+              E-mail:&nbsp;&nbsp;&nbsp;
+            </Typography>
+            {user.email}
+          </Typography>
+        )}
       </Container>
     </>
   );
