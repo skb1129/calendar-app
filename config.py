0c473183671b57f68107f2f95ecf08f54c3bf35a
Surya Kant Bansal <suryakantbansal97@gmail.com>
2020-04-18 19:42:46 +0530

    get database url from environment

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = "postgresql://kbthjwbmiitxup:42424862986ca4d7140a5978da9cb2a46107ef135c22b2d400394629308bba92@ec2-18-210-51-239.compute-1.amazonaws.com:5432/d5ua9i6m4tsfbs"
+SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
